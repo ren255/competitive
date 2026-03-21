696193b4436f7a4329a1ac15a65e3a4a4aa999e3
ren255 <renpetertoda@gmail.com>
2026-03-22 08:28:40 +0900

    abc450c

--- atcode/abc450C.py
+++ atcode/abc450C.py
@@ -1,30 +1,25 @@
-import bisect, collections, copy, heapq, itertools, math, numpy, string
+import bisect, collections, copy, heapq, itertools, math, string
 import sys
 
 
-def I():  # 1行に整数1つ
+def I():
     return int(sys.stdin.readline().rstrip())
 
 
-def LI():  # 1行に空白区切りの整数複数
+def LI():
     return list(map(int, sys.stdin.readline().rstrip().split()))
 
 
-def S():  # 1行に文字列1つ
+def S():
     return sys.stdin.readline().rstrip()
 
 
-def LS():  # 1行に空白区切りの文字列複数
+def LS():
     return list(sys.stdin.readline().rstrip().split())
 
 
 H, W = LI()
 MAP = [S() for _ in range(H)]
-
-
-def is_adjacent(pos1, pos2):
-    """2 つのマス(i,j),(i′ ,j′ ) が隣接しているとは、∣i−i′ ∣+∣j−j′ ∣=1 であることと定めます。"""
-    return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1]) == 1
 
 
 from collections import deque
@@ -38,8 +33,7 @@ class Pos:
     def __str__(self):
         return MAP[self.row - 1][self.col - 1]
 
-    def is_adjacent(self, pos: Pos):
-        """2 つのマス(i,j),(i′ ,j′ ) が隣接しているとは、∣i−i′ ∣+∣j−j′ ∣=1 であることと定めます。"""
+    def is_adjacent(self, pos):
         return abs(pos.col - self.col) + abs(pos.row - self.row) == 1
 
     def is_edge(self):
@@ -59,12 +53,12 @@ class Pos:
         return Pos(self.col - 1, self.row)
 
     def right(self):
-        if self.col + 1 >= W:
+        if self.col + 1 > W:
             return None
         return Pos(self.col + 1, self.row)
 
     def down(self):
-        if self.row + 1 >= H:
+        if self.row + 1 > H:
             return None
         return Pos(self.col, self.row + 1)
 
@@ -94,7 +88,7 @@ class Pos:
 
         return white
 
-    def __eq__(self, other: Pos):
+    def __eq__(self, other):
         return self.row == other.row and self.col == other.col
 
     def __hash__(self):
@@ -109,17 +103,17 @@ for col in range(1, W + 1):
     for row in range(1, H + 1):
         pos = Pos(col, row)
         if str(pos) == ".":
-            if pos.is_edge():
-                ps = tuple(pos.get_grupe())
-                edge_white.add(ps)
-                print(f"found edge white len:{len(ps)}")
+            if pos in in_white:
+                continue
+            grupe = pos.get_grupe()
+            is_edge_grupe = any(p.is_edge() for p in grupe)
+
+            for p in grupe:
+                in_white.add(p)
+            if is_edge_grupe:
+                edge_white.add(tuple(grupe))
             else:
-                if pos not in in_white:
-                    grupe_n += 1
-                ps = tuple(pos.get_grupe())
-                in_white.add(ps)
-                print(f"found in white len:{len(ps)}. and new:{pos not in in_white}")
+                grupe_n += 1
 
 
-print()
-print(grupe_n, len(edge_white))
+print(grupe_n)
